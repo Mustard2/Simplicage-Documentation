bb329adfa0229605a9909aaf3b69d7f17179bc7a
Mustard <60780948+Mustard2@users.noreply.github.com>
2022-01-14 19:58:28 +0100

    Change project settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Simplicage'
+copyright = '2022, Morelewd and Mustard'
+author = 'Morelewd and Mustard'
 
 release = '0.1'
 version = '0.1.0'
